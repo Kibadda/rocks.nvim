e9b1d047a7f68024dbc4b78a29453aa69740ad2e
Michael Strobel <mstrobel97@gmail.com>
2024-04-30 09:27:19 +0200

    refactor: change lsp attach autocmd

--- lua/me/lsp/attach.lua
+++ lua/me/lsp/attach.lua
@@ -2,6 +2,12 @@ local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 local clear = vim.api.nvim_clear_autocmds
 
+local groups = {
+  highlight = augroup("LspAttachHighlight", { clear = false }),
+  codelens = augroup("LspAttachCodelens", { clear = false }),
+  inlay = augroup("LspAttachInlay", { clear = false }),
+}
+
 autocmd("LspAttach", {
   group = augroup("LspAttach", { clear = true }),
   callback = function(args)
@@ -12,53 +18,130 @@ autocmd("LspAttach", {
       return
     end
 
-    local function map(lhs, rhs, desc, mode)
-      vim.keymap.set(mode or "n", lhs, rhs, { desc = desc, buffer = bufnr })
-    end
-
-    local groups = {
-      highlight = augroup("LspAttachHighlight", { clear = false }),
-      -- codelens = augroup("LspAttachHighlight", { clear = false }),
-      -- inlay = augroup("LspAttachHighlight", { clear = false }),
-    }
-
     local methods = vim.lsp.protocol.Methods
 
-    if client.supports_method(methods.textDocument_documentHighlight) then
-      clear { group = groups.highlight, buffer = bufnr }
-      autocmd({ "CursorHold", "InsertLeave", "BufEnter" }, {
-        group = groups.highlight,
-        buffer = bufnr,
-        callback = vim.lsp.buf.document_highlight,
-      })
-      autocmd({ "CursorMoved", "InsertEnter", "BufLeave" }, {
-        group = groups.highlight,
-        buffer = bufnr,
-        callback = vim.lsp.buf.clear_references,
-      })
-    end
+    ---@type table<string, { method?: string, lhs?: string, rhs?: function, mode?: string, desc?: string, extra?: function }>
+    local maps = {
+      {
+        method = methods.textDocument_definition,
+        lhs = "gd",
+        rhs = vim.lsp.buf.definition,
+        desc = "Definition",
+      },
+      {
+        method = methods.textDocument_references,
+        lhs = "gr",
+        rhs = function()
+          vim.lsp.buf.references {
+            includeDeclaration = false,
+          }
+        end,
+        desc = "References",
+      },
+      {
+        method = methods.textDocument_implementation,
+        lhs = "gI",
+        rhs = vim.lsp.buf.implementation,
+        desc = "Implementations",
+      },
+      {
+        method = methods.textDocument_declaration,
+        lhs = "gD",
+        rhs = vim.lsp.buf.declaration,
+        desc = "Declaration",
+      },
+      {
+        method = methods.textDocument_rename,
+        lhs = "crn",
+        rhs = vim.lsp.buf.rename,
+        desc = "Rename",
+      },
+      {
+        method = methods.textDocument_codeAction,
+        lhs = "crr",
+        rhs = vim.lsp.buf.code_action,
+        desc = "Code Action",
+      },
+      {
+        method = methods.textDocument_signatureHelp,
+        lhs = "<C-s>",
+        rhs = vim.lsp.buf.signature_help,
+        mode = "i",
+        desc = "Signature Help",
+      },
+      {
+        method = methods.textDocument_documentSymbol,
+        lhs = "gs",
+        rhs = vim.lsp.buf.document_symbol,
+        desc = "Document Symbols",
+      },
+      {
+        method = methods.workspace_symbol,
+        lhs = "gS",
+        rhs = vim.lsp.buf.workspace_symbol,
+        desc = "Workspace Symbols",
+      },
+      {
+        method = methods.textDocument_codeLens,
+        lhs = "gl",
+        rhs = vim.lsp.codelens.run,
+        desc = "Run Codelens",
+        extra = function()
+          clear { group = groups.codelens, buffer = bufnr }
+          autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
+            group = groups.codelens,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.codelens.refresh { bufnr = bufnr }
+            end,
+          })
+        end,
+      },
+      {
+        method = methods.textDocument_documentHighlight,
+        extra = function()
+          clear { group = groups.highlight, buffer = bufnr }
+          autocmd({ "CursorHold", "InsertLeave", "BufEnter" }, {
+            group = groups.highlight,
+            buffer = bufnr,
+            callback = vim.lsp.buf.document_highlight,
+          })
+          autocmd({ "CursorMoved", "InsertEnter", "BufLeave" }, {
+            group = groups.highlight,
+            buffer = bufnr,
+            callback = vim.lsp.buf.clear_references,
+          })
+        end,
+      },
+      {
+        method = methods.textDocument_inlayHint,
+        lhs = "gL",
+        rhs = function()
+          vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled(bufnr), { bufnr = bufnr })
+        end,
+        desc = "Toggle Inlay Hint",
+      },
+      {
+        method = methods.textDocument_formatting,
+        extra = function()
+          vim.b[bufnr].formatter = vim.lsp.buf.format
+        end,
+      },
+    }
 
-    if client.supports_method(methods.textDocument_rename) then
-      map("crr", vim.lsp.buf.rename, "Rename")
-    end
+    for _, mapping in ipairs(maps) do
+      if not mapping.method or client.supports_method(mapping.method) then
+        if mapping.lhs then
+          vim.keymap.set(mapping.mode or "n", mapping.lhs, mapping.rhs, {
+            buffer = bufnr,
+            desc = mapping.desc,
+          })
+        end
 
-    if client.supports_method(methods.textDocument_signatureHelp) then
-      map("<C-s>", vim.lsp.buf.signature_help, "Signature Help", "i")
+        if mapping.extra then
+          mapping.extra()
+        end
+      end
     end
-
-    if client.supports_method(methods.textDocument_formatting) and not vim.b[bufnr].formatter then
-      vim.b[bufnr].formatter = vim.lsp.buf.format
-    end
-
-    if client.supports_method(methods.textDocument_definition) then
-      map("gd", vim.lsp.buf.definition, "Go to Definition")
-    end
-
-    if client.supports_method(methods.textDocument_documentSymbol) then
-      map("<Leader>ls", vim.lsp.buf.document_symbol, "Symbols")
-    end
-
-    map("<Leader>lj", vim.diagnostic.goto_next, "Next Diagnostic")
-    map("<Leader>lk", vim.diagnostic.goto_prev, "Prev Diagnostic")
   end,
 })
