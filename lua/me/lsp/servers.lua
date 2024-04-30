b82090f5417e7b3a371a80a88e21e657642fbe34
Michael Strobel <mstrobel97@gmail.com>
2024-04-30 13:52:24 +0200

    feat: add intelephense and typescript-language-server

--- lua/me/lsp/servers.lua
+++ lua/me/lsp/servers.lua
@@ -55,6 +55,52 @@ local servers = {
       },
     },
   },
+
+  {
+    filetypes = { "php" },
+    root_markers = { "composer.json", ".git" },
+    config = {
+      cmd = { "intelephense", "--stdio" },
+      settings = {
+        intelephense = {
+          -- stylua: ignore
+          stubs = {
+            "apache", "apcu", "bcmath", "bz2", "calendar", "com_dotnet", "Core", "ctype", "curl",
+            "date", "dba", "dom", "enchant", "exif", "FFI", "fileinfo", "filter", "fpm", "ftp",
+            "gd", "gettext", "gmp", "hash", "iconv", "imap", "intl", "json", "ldap", "libxml",
+            "mbstring", "meta", "mysqli", "oci8", "odbc", "openssl", "pcntl", "pcre", "PDO",
+            "pdo_ibm", "pdo_mysql", "pdo_pgsql", "pdo_sqlite", "pgsql", "Phar", "posix", "pspell",
+            "readline", "Reflection", "session", "shmop", "SimpleXML", "snmp", "soap", "sockets",
+            "sodium", "SPL", "sqlite3", "standard", "superglobals", "sysvmsg", "sysvsem",
+            "sysvshm", "tidy", "tokenizer", "xml", "xmlreader", "xmlrpc", "xmlwriter", "xsl",
+            "Zend OPcache", "zip", "zlib", "wordpress", "phpunit",
+          },
+          format = {
+            braces = "psr12",
+          },
+          phpdoc = {
+            textFormat = "text",
+            functionTemplate = {
+              summary = "$1",
+              tags = {
+                "@param ${1:$SYMBOL_TYPE} $SYMBOL_NAME",
+                "@return ${1:$SYMBOL_TYPE}",
+                "@throws ${1:$SYMBOL_TYPE}",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+
+  {
+    filetypes = { "javascript", "typescript" },
+    root_markers = { "package.json" },
+    config = {
+      cmd = { "typescript-language-server", "--stdio" },
+    },
+  },
 }
 
 local lsp = require "me.lsp"
