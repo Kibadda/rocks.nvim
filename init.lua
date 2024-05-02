51a6e74de82a68d52812672b38fe76dda1bb789f
Michael Strobel <mstrobel97@gmail.com>
2024-05-02 08:59:32 +0200

    feat: remove providers

--- init.lua
+++ init.lua
@@ -22,3 +22,7 @@ end
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_python3_provider = 1
+vim.g.loaded_node_provider = 1
+vim.g.loaded_perl_provider = 1
+vim.g.loaded_ruby_provider = 1
