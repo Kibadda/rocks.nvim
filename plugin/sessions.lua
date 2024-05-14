6d88aef8a93f5e3a8dd4af77c66c5bfcdbc71b93
Michael Strobel <mstrobel97@gmail.com>
2024-05-14 08:25:08 +0200

    feat(session): add session name to kitty tab name

--- plugin/sessions.lua
+++ plugin/sessions.lua
@@ -64,9 +64,40 @@ local function delete()
   end
 end
 
+local group = vim.api.nvim_create_augroup("Session", { clear = true })
+
 vim.api.nvim_create_autocmd("VimLeavePre", {
-  group = vim.api.nvim_create_augroup("Session", { clear = true }),
-  callback = update,
+  group = group,
+  callback = function()
+    update()
+  end,
+})
+
+local function kitty(title)
+  vim.system {
+    "kitty",
+    "@",
+    "--to",
+    vim.env.KITTY_LISTEN_ON,
+    "set-tab-title",
+    title and "nvim " .. title or "",
+  }
+end
+
+vim.api.nvim_create_autocmd({ "VimLeavePre", "FocusLost" }, {
+  group = group,
+  callback = function()
+    kitty()
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "FocusGained", "SessionLoadPost" }, {
+  group = group,
+  callback = function()
+    if vim.v.this_session and vim.v.this_session ~= "" then
+      kitty(vim.fs.basename(vim.v.this_session))
+    end
+  end,
 })
 
 vim.keymap.set("n", "<Leader>Sn", new, { desc = "New" })
