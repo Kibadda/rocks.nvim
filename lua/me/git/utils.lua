0939d07af5bdce25ae315d3ee08b5066a69140b2
Michael Strobel <mstrobel97@gmail.com>
2024-05-24 13:44:56 +0200

    refactor(git): move log post_run to utils

--- lua/me/git/utils.lua
+++ lua/me/git/utils.lua
@@ -115,4 +115,90 @@ function M.select(opts)
   return choice
 end
 
+function M.open_split(opts)
+  local bufnr = vim.api.nvim_create_buf(false, false)
+  vim.api.nvim_buf_set_name(bufnr, opts.name or "GIT")
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, opts.lines or {})
+  vim.bo[bufnr].modifiable = false
+  vim.bo[bufnr].modified = false
+  vim.bo[bufnr].bufhidden = "wipe"
+
+  if opts.ft then
+    vim.bo[bufnr].filetype = opts.ft
+  end
+
+  local ns = vim.api.nvim_create_namespace("git" .. bufnr)
+
+  for _, extmark in ipairs(opts.extmarks or {}) do
+    vim.api.nvim_buf_set_extmark(bufnr, ns, extmark.line - 1, extmark.col - 1, {
+      end_col = extmark.end_col,
+      hl_group = extmark.hl,
+    })
+  end
+
+  local win = vim.api.nvim_open_win(bufnr, opts.enter ~= false, {
+    split = opts.split or "below",
+    win = 0,
+    height = (not opts.split or opts.split == "below" or opts.split == "above") and opts.size or nil,
+    width = (opts.split == "left" or opts.split == "right") and opts.size or nil,
+  })
+
+  vim.keymap.set("n", "q", function()
+    vim.api.nvim_buf_delete(bufnr, { force = true })
+  end, { buffer = bufnr })
+
+  return win, bufnr
+end
+
+function M.create_log_buffer(lines)
+  local extmarks = {}
+
+  for i, line in ipairs(lines) do
+    local full_line, hash, branch, date
+
+    full_line, _, hash, branch, date = line:find "^([^%s]+) %- (%([^%)]+%)).*(%([^%)]+%))$"
+
+    if not full_line then
+      full_line, _, hash, date = line:find "^([^%s]+) %-.*(%([^%)]+%))$"
+    end
+
+    if not full_line then
+      break
+    end
+
+    table.insert(extmarks, { line = i, col = 1, end_col = #hash, hl = "Red" })
+    if branch then
+      table.insert(extmarks, { line = i, col = #hash + 3, end_col = #hash + 3 + #branch, hl = "Yellow" })
+    end
+    table.insert(extmarks, { line = i, col = #line - #date, end_col = #line, hl = "Green" })
+  end
+
+  local _, bufnr = M.open_split {
+    name = "GIT LOG",
+    lines = lines,
+    extmarks = extmarks,
+  }
+
+  vim.keymap.set("n", "<CR>", function()
+    local row = vim.api.nvim_win_get_cursor(0)[1]
+    local line = vim.api.nvim_buf_get_lines(bufnr, row - 1, row, false)[1]
+    local hash = line:match "^([^%s]+)"
+    local difflines = M.git_command { "diff", string.format("%s^..%s", hash, hash) }
+
+    M.create_diff_buffer(difflines)
+  end)
+end
+
+function M.create_diff_buffer(lines)
+  local _, bufnr = M.open_split {
+    name = "GIT DIFF",
+    lines = lines,
+    ft = "diff",
+  }
+
+  vim.keymap.set("n", "<CR>", function()
+    vim.api.nvim_buf_delete(bufnr, { force = true })
+  end, { buffer = bufnr })
+end
+
 return M
