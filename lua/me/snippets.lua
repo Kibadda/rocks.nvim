177bec5b892439c8a2b80dd28bc86fc7376541cf
Michael Strobel <mstrobel97@gmail.com>
2024-05-27 08:59:18 +0200

    feat(snippets): add new php snippets

--- lua/me/snippets.lua
+++ lua/me/snippets.lua
@@ -6,6 +6,11 @@ local default = {
   end,
 }
 
+local function get_filename()
+  local bufname = vim.fs.basename(vim.api.nvim_buf_get_name(0))
+  return vim.split(bufname, "%.")[1]
+end
+
 local snippets = {
   javascript = {
     log = "console.${0:log}($1);",
@@ -29,6 +34,45 @@ local snippets = {
     foreach = "foreach ($1 as $2) {\n\t$0\n}",
 
     try = "try {\n\t$0\n} catch ($1) {\n\t$2\n}",
+
+    class = function()
+      return string.format("${1|abstract ,final |}class %s$2 {\n\t$0\n}", get_filename())
+    end,
+    enum = function()
+      return string.format("enum %s$1 {\n\t$0\n}", get_filename())
+    end,
+    interface = function()
+      return string.format("interface %s$1 {\n\t$0\n}", get_filename())
+    end,
+    trait = function()
+      return string.format("trait %s$1 {\n\t$0\n}", get_filename())
+    end,
+
+    fun = "${1|public ,protected ,private |}${2|static |}function $3($4)$5 {\n\t$0\n}",
+
+    getset = function()
+      local property
+
+      vim.ui.input({
+        prompt = "Property name: ",
+      }, function(choice)
+        property = choice
+      end)
+
+      if not property or property == "" then
+        return ""
+      end
+
+      return string.format(
+        "public function get%s(): $1 {\n\treturn \\$this->%s;\n}\n\npublic function set%s($1 \\$%s): void {\n\t\\$this->%s = \\$%s;\n}",
+        property:gsub("^%l", string.upper),
+        property,
+        property:gsub("^%l", string.upper),
+        property,
+        property,
+        property
+      )
+    end,
   },
 }
 
